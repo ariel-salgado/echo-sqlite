8c77040e86187dd15abceb2086eb851358224183
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-01-19 19:57:23 -0300

    feat: load .env variables to set echo server port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	err := godotenv.Load()
+
+  	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+  
 	e := echo.New()
+	e.HideBanner = true
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
 	
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
